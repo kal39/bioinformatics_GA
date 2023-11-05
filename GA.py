2fb1893dc396cfe622ce85b44c156eaca19a46b4
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2023-11-05 13:22:27 +0100

    a bit better now

--- GA.py
+++ GA.py
@@ -3,33 +3,41 @@ import numpy as np
 
 class Individual:
     # for generating a new individual from a sequence (used to make generation 0)
-    def new(seq: list[str], w_gap: int, w_mismatch: int) -> "Individual":
+    def new(seq: list[str], w_gap: int, w_match: int) -> "Individual":
         self = Individual()
         self.w_gap = w_gap
-        self.w_mismatch = w_mismatch
+        self.w_match = w_match
         self.seq = np.array([bytearray(s, "utf-8") for s in seq], dtype=int)  # convert string to ascii array
         self.gap = np.zeros(self.seq.shape, dtype=int)  # initialize gaps to 0
         return self
 
     # for generating a new individual from two parents (for crossover)
     def from_parents(p1: "Individual", p2: "Individual") -> "Individual":
-        self = Individual()
-        self.w_gap = p1.w_gap
-        self.w_mismatch = p1.w_mismatch
-        self.seq = p1.seq
-        gap_1 = p1.gap[:, :p1.gap.shape[1] // 2]  # first half from p1
-        gap_2 = p2.gap[:, p1.gap.shape[1] // 2:]  # second half from p2
-        self.gap = np.hstack([gap_1, gap_2])  # combine the two halves
-
-        return self
+        child_1 = Individual()
+        child_2 = Individual()
+        child_1.w_gap = p1.w_gap
+        child_1.w_match = p1.w_match
+        child_1.seq = p1.seq
+        child_2.w_gap = p1.w_gap
+        child_2.w_match = p1.w_match
+        child_2.seq = p1.seq
+        gap_1_1 = p1.gap[:, :p1.gap.shape[1] // 2]  # first half from p1
+        gap_2_1 = p2.gap[:, p1.gap.shape[1] // 2:]  # second half from p2
+        gap_1_2 = p2.gap[:, :p1.gap.shape[1] // 2]  # first half from p2
+        gap_2_2 = p1.gap[:, p1.gap.shape[1] // 2:]  # second half from p1
+        child_1.gap = np.hstack([gap_1_1, gap_2_1])  # combine the two halves
+        child_2.gap = np.hstack([gap_1_2, gap_2_2])  # same for child 2
+        return child_1 if child_1.evaluate()[0] >= child_2.evaluate()[0] else child_2
 
     def evaluate(self) -> float:
         matches = sum([
             max([*[x[1] for x in zip(*np.unique(col, return_counts=True)) if chr(x[0]) != "-"], 1]) - 1
             for col in self._cur_seq().T
         ])
-        # gaps = self.gap.sum(axis=1).max() * self.gap.shape[0]
-        return matches
+        gaps = 0
+        for seq in self._cur_seq():
+            gaps += np.sum(seq == 45)
+        return [matches * self.w_match - (gaps * self.w_gap), matches, gaps]
 
     def mutate(self, p: float) -> None:
         idx = np.random.rand(*self.gap.shape) < p  # select indices to mutate
@@ -39,7 +47,7 @@ class Individual:
         return np.random.randint(0, self.seq.shape[1])
 
     # get the sequence the individual currently represents (including the gaps)
-    def _cur_seq(self) -> list[str]:
+    def _cur_seq(self) -> np.ndarray:
         max_len = self.gap.sum(axis=1).max()  # maximum gap count (for padding)
         stacked = np.dstack([self.seq, self.gap])  # combine seq and gap arrays, so its easier to iterate
         raw_out = np.array([
@@ -48,17 +56,17 @@ class Individual:
                 "-" * (max_len - seq[:, 1].sum()), "utf-8"  # pad with '-'s
             ) for seq in stacked
         ], dtype=int)  # convert string back to ascii int array
-
         return np.array([col for col in raw_out.T if "".join([chr(c) for c in col]).strip("-") != ""]).T
 
     # pretty print
     def __str__(self) -> str:
-        return f"Score: {self.evaluate()}\n" + "\n".join(["".join([chr(c) for c in seq]) for seq in self._cur_seq()]) + "\n"
+        scores = self.evaluate()
+        return f"Score: {scores[0]}\n" + f"Matches: {scores[1]}\n" + f"Gaps: {scores[2]}\n"+"\n".join(["".join([chr(c) for c in seq]) for seq in self._cur_seq()]) + "\n"
 
 
 class Population:
-    def __init__(self, seq: list[str], w_gap: int, w_mismatch: int, pop_count: int) -> None:
-        self.individuals = [Individual.new(seq, w_gap, w_mismatch) for _ in range(pop_count)]  # init individuals
+    def __init__(self, seq: list[str], w_gap: int, w_match: int, pop_count: int) -> None:
+        self.individuals = [Individual.new(seq, w_gap, w_match) for _ in range(pop_count)]  # init individuals
 
     def clear_individuals(self):
         self.individuals.clear()
@@ -71,7 +79,9 @@ class Population:
         orig_len = len(self.individuals)
 
         # Roulette Selection
-        p = np.array([ind.evaluate() for ind in self.individuals])
+        p = np.array([ind.evaluate()[0] for ind in self.individuals])
+        if np.any(p < 0):
+            p = p + abs((min(p))) + 1
         p = p / p.sum() if p.sum() != 0 else np.ones(p.shape[0]) / p.shape[0]
 
         new_individuals = []
@@ -95,14 +105,15 @@ class Population:
 
 # some similar-ish sequences (TODO: probably a horrible example)
 sequences = [
-    "AAA1111CC",
-    "22AAACC22",
-    "333333AAA",
+    "AAATTTTCC",
+    "GGAAACCGG",
+    "UUUUUUAAA"
 ]
 
-pop = Population(sequences, 1, 1, 100)
+pop = Population(sequences, 0.05, 1, 100)
 
-for i in range(1000):
+for i in range(100):
+    # print("generation: " + str(i))
     print(pop)
     pop.next_gen()
-    pop.mutate(0.01)
+    pop.mutate(0.05)
